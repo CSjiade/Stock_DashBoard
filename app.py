821e720f3b2618fb195eb253fa9592e2291b3a0e
Lian Jiade <jdlian505@gmail.com>
2023-02-16 00:03:36 +0800

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ if __name__ == '__main__':
     #Stock Overview Section
     st.image(img, use_column_width=True)
     st.sidebar.header('Enter your Inputs')
-    stock_ticker = st.sidebar.text_input("Stock Ticker", "TSM")
+    stock_ticker = st.sidebar.text_input("Stock Ticker", "AAPL")
     period_view = st.sidebar.checkbox('period')
     mv_slow = st.sidebar.text_input("Moving Average (Slow)",5)
     mv_fast = st.sidebar.text_input("Moving Average (Fast)",25)
@@ -42,7 +42,7 @@ if __name__ == '__main__':
 
         st.sidebar.header('Backtest Strategy')
         st.title("BackTest")
-        ticker = st.sidebar.text_input("Ticker","TSM")
+        ticker = st.sidebar.text_input("Ticker","AAPL")
         stock_bt = yf.Ticker(ticker)
         stock_close = stock_bt.history(period = "5d", interval = "1d")
         current_price = st.sidebar.text_input( "Current Price" , "%.2f" %stock_close.Close[-1])
